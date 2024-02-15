25bf86773a3af61f06716ff175ddc770047c3524
mal1ceee <98943564+mal1ceee@users.noreply.github.com>
2024-02-15 15:41:21 +0800

    Querying from database + trying to switch to linux
    implementing dummy database to test querying

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	// "fmt"
-	"github.com/mal1ceee/GolangApIGovTech/internal/repository"
+	"GOLANGAPIGOVTECH/internal/repository"
 	// "net/http"
 	// "GolangApIGovTech/internal/config"
 	// "GolangApIGovTech/internal/handler"
